6d7d8cbb792770f4d3e0d61e37a8c4066c10a1b5
Adilson Fernando <ornellas00@hotmail.com>
2023-11-02 14:59:49 -0300

    feat: cards Produtos ja com styles

--- project-serraTec-2023.2/src/components/CardProdutos/CardProduto.jsx
+++ project-serraTec-2023.2/src/components/CardProdutos/CardProduto.jsx
@@ -4,12 +4,16 @@ const CardProduto = ({id,nome,preco,quantidade,descricao,imgurl}) => {
 
     return (
     <div className="card" key={id}>
+      <div className='conteinerImgProd'>
         <img className="imgcard" src={imgurl} />
-          <p className="infoProd">{nome}</p>
-          <p className="infoProd">{preco}</p>
-          <p className="infoProd">{descricao}</p>
-          <p className="infoProd">{quantidade}</p>
-          <button className="buttonCompra">Comprar</button>{' '}
+      </div>
+      <div className='containerInfoProd'>
+        <p className="nomeProd">{nome}</p>
+        <p className="precoProd">R$: {preco}</p>
+        <p className="descricaoProd">{descricao}</p>
+        <p className="quantidadeProd">Quantidade restante: {quantidade}</p>
+      </div>
+      <button className="buttonCompra">Comprar</button>
     </div>
   );
 }
