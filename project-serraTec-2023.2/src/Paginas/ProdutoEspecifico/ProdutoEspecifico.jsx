288affe32d91229b611dd30c392db741d92512ed
Adilson Fernando <ornellas00@hotmail.com>
2023-11-02 21:06:49 -0300

    fix: correção do loop que tava dando na pagina de produto p pagina de produto especifico

--- project-serraTec-2023.2/src/Paginas/ProdutoEspecifico/ProdutoEspecifico.jsx
+++ project-serraTec-2023.2/src/Paginas/ProdutoEspecifico/ProdutoEspecifico.jsx
@@ -1,6 +1,5 @@
 import { useParams } from "react-router-dom";
 import NavBar from "../../components/NavBar/NavBar"
-import Header from "../../components/header/header";
 
 const ProdutoEspecifico = () => {  
   const { id } = useParams()
@@ -9,7 +8,6 @@ const ProdutoEspecifico = () => {
     return (
       <>
         <NavBar/>
-        <Header/>
         Pagina do Produto {id}
       </>
     );
