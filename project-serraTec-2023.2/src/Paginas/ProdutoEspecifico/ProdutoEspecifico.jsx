4fd8f08532a769a9c89f699647a60df26defbf85
Adilson Fernando <ornellas00@hotmail.com>
2023-11-07 13:08:53 -0300

    feat: subindo com a pagina de produto especifico que a TAYNARA fez

--- project-serraTec-2023.2/src/Paginas/ProdutoEspecifico/ProdutoEspecifico.jsx
+++ project-serraTec-2023.2/src/Paginas/ProdutoEspecifico/ProdutoEspecifico.jsx
@@ -1,18 +1,52 @@
 import { useParams } from "react-router-dom";
 import { api } from "../../api/api"
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import NavBar from "../../components/NavBar/NavBar";
 import "./ProdutoEspecifico.css";
 import Footer from "../../components/Footer/Footer";
+import { AuthContext } from "../../Contexto/Context";
 
-const ProdutoEspecifico = () => {  
+const ProdutoEspecifico = () => {
+  const { listaProduto, setListaCarrinho, listaCarrinho, setTemListaCarrinho} = useContext(AuthContext);
   const { id } = useParams()
   const [produto, setProduto] = useState("");
+  const [quantidadeProd, setQuantidadeProd] = useState(0);  
+  const [mensagemQuantidadeMaxPermitida, setMensagemQuantidadeMaxPermitida] = useState("");
 
   const getProdutoEspecifico = async () => {
     const response = await api.get("/produtos/"+ id);
     setProduto(response.data);
   };
+
+  const adicinarAoCarrinho = (id) => {
+    if (quantidadeProd === 0) {
+    console.log(id);
+    } else {
+   
+      const produtoSelecionado = listaProduto.find((prod) => prod.id === id);
+      const produtoASerAdicionado = { ...produtoSelecionado, quantidadeProd };
+      setListaCarrinho([...listaCarrinho, produtoASerAdicionado]);
+      setTemListaCarrinho(true)
+    }  
+  }
+
+  function diminuirQuantidade() {
+    if (quantidadeProd <= 0) {
+
+    } else {
+      setMensagemQuantidadeMaxPermitida("");
+      setQuantidadeProd(quantidadeProd - 1);
+    }
+  }
+
+function aumentarQuantidade() {
+    if (quantidadeProd >= produto.estoque) {
+      setMensagemQuantidadeMaxPermitida("Quantidade máxima em Estoque");
+    } else {
+      setQuantidadeProd(quantidadeProd + 1);
+      setMensagemQuantidadeMaxPermitida("");
+    }
+  }
   
   useEffect(() => {
     getProdutoEspecifico();
@@ -21,21 +55,31 @@ const ProdutoEspecifico = () => {
     return (
       <>
         <NavBar/>
-        <div className="containerPaginaProdEspecifico">
-            <div className="containerImgProdEspecifico">
-                <img className="imgProd" src={produto.imgurl} alt="Imagem do Produto" />
+        <div className="cardProduto">
+          <div className="imagemContainer">
+            <img className="imgProduto" src={produto.imgurl} alt="Imagem do Produto" />
+          </div>
+          <div className="informacoesContainer">
+            <h2 className="nomeProd">{produto.nome}</h2>
+            <h1 className="preçoProd">R$:{produto.preco}</h1>
+            <div className="containerQuantidadeAndMensagem">
+              <div className='conteinerButtonquantidade' >
+                <button className="buttonAlterarQuantidade" onClick={diminuirQuantidade}> - </button>
+                <h6 className='textQuantidadeAlterada'> {quantidadeProd} </h6>
+                <button className="buttonAlterarQuantidade" onClick={aumentarQuantidade}> + </button>
+              </div>
+              <h6>{mensagemQuantidadeMaxPermitida}</h6>
             </div>
-            <div className="containerInfoProd">
-              <h2 className="nomeProd">{produto.nome}</h2>
-              <h1 className="preçoProd">R$:{produto.preco}</h1>
-              <h3 className="qtdProd">Estoque disponivel Restante: {produto.estoque}</h3>
-              <button className="buttonCompra">Comprar</button>
+            <h6 className="qtdProd">Estoque disponível Restante: {produto.estoque}</h6>
+            <div className="containerDescricao">
+              <p className="tituloContainerDescricao">Descrição do Produto:</p>
+              <p className="descricao">{produto.descricao}</p>
             </div>
-        </div>
-        <div className="containerDescricao">
-          <p className="tituloContainerDescricao">Descrição do Produto:</p>
-          <p className="descricao">{produto.descricao}</p>
-        </div>
+            <div className="botoesAcao">
+              <button className="btnAdicionarCarrinho"onClick={() => adicinarAoCarrinho(produto.id)}>Adicionar ao Carrinho</button>
+            </div>
+          </div>
+          </div>
         <Footer/>
       </>
     );
