5a72cd3b6182c8b928d27d976189d2f3892a2407
Adilson Fernando <ornellas00@hotmail.com>
2023-11-06 22:19:00 -0300

    feat: link api modificada p deploy vercel

--- project-serraTec-2023.2/src/api/api.js
+++ project-serraTec-2023.2/src/api/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-    baseURL: 'http://localhost:8080/'
+    baseURL: 'https://deploy-db-json-vercel-react-serra-tec2023-3.vercel.app/'
 })
